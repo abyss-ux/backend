9e2424538e1d55369d70dcf50c103c89b0974eb0
Tushar Karki <tusharkarki1606@gmail.com>
2026-02-10 00:39:21 +0530

    json data added

--- index.js
+++ index.js
@@ -20,7 +20,38 @@ app.get('/google',(req,res)=>{
 app.get('/youtube',(req,res)=>{
     res.send('This is Youtube')
 })
+const jsonData = {
+  "users": [
+    {
+      "id": 1,
+      "name": "Aarav Sharma",
+      "email": "aarav.sharma@example.com",
+      "age": 21,
+      "role": "user",
+      "isActive": true
+    },
+    {
+      "id": 2,
+      "name": "Priya Verma",
+      "email": "priya.verma@example.com",
+      "age": 23,
+      "role": "admin",
+      "isActive": true
+    },
+    {
+      "id": 3,
+      "name": "Rohan Mehta",
+      "email": "rohan.mehta@example.com",
+      "age": 20,
+      "role": "user",
+      "isActive": false
+    }
+  ]
+}
 
+app.get('/json',(req,res)=>{
+    res.json(jsonData)
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
